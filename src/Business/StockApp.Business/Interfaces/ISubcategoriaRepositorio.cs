730d12d65664c26502819a026f536e14ed0312ef
Danillo Lopes <lopessousa2004@gmail.com>
2023-12-01 20:11:15 -0300

    Feat: Adição da interface

---
+++ src/Business/StockApp.Business/Interfaces/ISubcategoriaRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using StockApp.Business.Abstract;
+using StockApp.Business.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Business.Interfaces
+{
+    public interface ISubcategoriaRepositorio 
+    {
+        bool Incluir(Categoria categoria);
+        bool Atualizar(Categoria categoria);
+        bool Remover(int Id);
+
+
+
+
+
+    }
+}
